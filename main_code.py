253e96c41efd8a73cc2722dc1324a57efa457ccc
xenoquarters <xenoquarters@users.noreply.github.com>
2020-10-07 13:04:39 +0800

    changed a bit of the logic and removed threading

--- main_code.py
+++ main_code.py
@@ -1,6 +1,8 @@
 from timeit import default_timer as timer
 import logging
 import threading
+
+
 # test_file = open("testing_file.txt", "a")
 # for i in range(10):
 #     test_file.write(str(i) + "\n")
@@ -20,33 +22,37 @@ def add_books():
 
         while borrowed_state.lower() not in ["yes", "no"]:
             borrowed_state = input("Has it been borrowed? (yes/no) ")
-        
-        borrowed_state == True if borrowed_state.lower() == "yes" else False
-                      
+
+        # double == makes the True/False ineffective
+        borrowed_state = True if borrowed_state.lower() == "yes" else False
+
         borrowed.write(f"{include_book}: {str(borrowed_state)}\n")
         borrowed.flush()
+
 
 def getList():
     readable_list = open("booklist.txt", "r")
     print("\nHere are the list of books: ")
     print(readable_list.read())
 
-def timeout():
-    start = timer()
-    if start == 3:
-        bool_booklist = input("Do you need the booklist? ")
-        if bool_booklist.lower() == "yes":
-            getList()
-        else:
-            pass
-        availability()
+
+# def timeout():
+#     start = timer()
+#     if start == 3:
+#         bool_booklist = input("Do you need the booklist? ")
+#         if bool_booklist.lower() == "yes":
+#             getList()
+#         else:
+#             pass
+#         availability()
+
 
 def availability():
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    logging.info("Ten seconds in, there will be a prompt for a booklist.")
-    avil_start = threading.Thread(target=timeout)
-    avil_start.start()
+    # time_format = "%(asctime)s: %(message)s"
+    # logging.basicConfig(format=time_format, level=logging.INFO, datefmt="%H:%M:%S")
+    # logging.info("Ten seconds in, there will be a prompt for a booklist.")
+    # avil_start = threading.Thread(target=timeout)
+    # avil_start.start()
     available = input("What book are you finding for? ")
     with open("booklist.txt") as check_availability:
         for num, line in enumerate(check_availability):
@@ -72,6 +78,7 @@ def availability():
         if boolean is None:
             print("There are no records of the book.")
 
+
 def main(task):
     if task == "0":
         add_books()
@@ -82,6 +89,7 @@ def main(task):
     else:
         print("No such function yet.")
 
+
 menu = ("hellllllooo-world:\n"
         "----------------------------------------\n"
         "Main Menu\n"
@@ -91,7 +99,7 @@ menu = ("hellllllooo-world:\n"
         "[2] Check Availability\n"
 
         "What do you want to do today? "
-)
+        )
 
 main(input(menu))
 
